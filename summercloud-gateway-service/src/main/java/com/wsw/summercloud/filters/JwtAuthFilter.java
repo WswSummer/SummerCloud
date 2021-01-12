dcd32279c3b09de2529f0e403d24734a1857306e
wangsongwen <2544894086wsw@gmail.com>
2021-01-12 17:53:31 +0800

    微服务网关优化

--- summercloud-gateway-service/src/main/java/com/wsw/summercloud/filters/JwtAuthFilter.java
+++ summercloud-gateway-service/src/main/java/com/wsw/summercloud/filters/JwtAuthFilter.java
@@ -1,6 +1,56 @@
-package com.wsw.summercloud.filters;/**
-* @Author WangSongWen
-* @Date: Created in 17:32 2021/1/12
-* @Description: 
-*/public class JwtAuthFilter {
+package com.wsw.summercloud.filters;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * @Author WangSongWen
+ * @Date: Created in 17:32 2021/1/12
+ * @Description:
+ */
+@Component
+@ConfigurationProperties("my.jwt")
+public class JwtAuthFilter implements GlobalFilter, Ordered {
+    private String[] skipAuthUrls;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String url = exchange.getRequest().getURI().getPath();
+
+        //跳过不需要验证的路径
+        if (null != skipAuthUrls && isSkipUrl(url)) {
+            return chain.filter(exchange);
+        }
+
+
+
+
+        return null;
+    }
+
+    /**
+     * 判断当前访问的url是否开头URI是在配置的忽略url列表中
+     *
+     * @param url
+     * @return
+     */
+    public boolean isSkipUrl(String url) {
+        for (String skipAuthUrl : skipAuthUrls) {
+            if (url.startsWith(skipAuthUrl)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // 优先级
+    @Override
+    public int getOrder() {
+        return -1;
+    }
 }
