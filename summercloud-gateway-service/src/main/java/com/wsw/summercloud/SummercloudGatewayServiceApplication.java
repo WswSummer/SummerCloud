30626f9dfafe4568ad854b83d2b94cdca018dc72
wangsongwen <2544894086wsw@gmail.com>
2021-01-12 14:28:36 +0800

    网关模块

--- summercloud-gateway-service/src/main/java/com/wsw/summercloud/SummercloudGatewayServiceApplication.java
+++ summercloud-gateway-service/src/main/java/com/wsw/summercloud/SummercloudGatewayServiceApplication.java
@@ -1,4 +1,4 @@
-package com.wsw.summercloudgatewayservice;
+package com.wsw.summercloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- summercloud-gateway-service/src/test/java/com/wsw/summercloud/SummercloudGatewayServiceApplicationTests.java
+++ summercloud-gateway-service/src/test/java/com/wsw/summercloud/SummercloudGatewayServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.wsw.summercloudgatewayservice;
+package com.wsw.summercloud;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
